0ea3c37d728744b8b4898372e58225d66298d1ae
gauravzgit <gauravsharma1428@gmail.com>
2018-09-25 10:30:28 +0530

    G-AUTH commit

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-    res.send({ hi : "there"})
-})
 
-app.listen(3001);
+passport.use(
+    new GoogleStrategy(
+        {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        },
+        (accessToken, refreshToken) => {
+            console.log('accessToken:',  accessToken);
+            console.log('refreshToken:',  refreshToken);
+        }
+    )
+);  
 
-console.log('Your app is running at port : 3001')
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+app.get('auth/google/callback', passport.authenticate('google'));
+
+app.listen(5003);
+console.log('Your app is running at port : 5003 || http://localhost:5003')
