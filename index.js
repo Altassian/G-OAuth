1fba4b77d34aa5099aa2eb03d6a199be808642b8
Altassian <43282023+Altassian@users.noreply.github.com>
2018-09-26 13:09:29 +0530

    Added missed slash('/') in get request for auth.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get(
         scope: ['profile', 'email']
     })
 );
-app.get('auth/google/callback', passport.authenticate('google'));
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(5003);
 console.log('Your app is running at port : 5003 || http://localhost:5003')
