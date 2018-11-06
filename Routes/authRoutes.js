f39105f8211a2aedf0f2c34fbec9902609eee3df
gauravzgit <gauravsharma1428@gmail.com>
2018-11-07 01:31:50 +0530

    aaa

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -5,6 +5,7 @@ app.get(
     '/auth/google',
     passport.authenticate('google', {
         scope: ['profile', 'email']
+        
     })
 );
 app.get('/auth/google/callback', passport.authenticate('google'));
